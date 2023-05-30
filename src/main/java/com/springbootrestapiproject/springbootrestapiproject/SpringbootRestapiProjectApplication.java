b49d9014a0bad95322934788186fc92b061d33e3
KumuthaS <KS00006@safeway.com>
2023-05-30 18:49:47 +0530

    Initial commit

---
+++ src/main/java/com/springbootrestapiproject/springbootrestapiproject/SpringbootRestapiProjectApplication.java
@@ -0,0 +1,14 @@
+package com.springbootrestapiproject.springbootrestapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiProjectApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/com/springbootrestapiproject/springbootrestapiproject/SpringbootRestapiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapiproject.springbootrestapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
